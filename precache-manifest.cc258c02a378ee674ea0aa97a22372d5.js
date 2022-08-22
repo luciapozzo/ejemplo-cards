4efa06a933df51d45baeb783e887b830990dc0f0
luciapozzo <luapozzo@gmail.com>
2022-08-22 19:20:52 -0300

    Updates

---
+++ precache-manifest.cc258c02a378ee674ea0aa97a22372d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "57eff5276f4c4a531696",
+    "url": "/ejemplo-cards/static/css/main.2d9946bd.chunk.css"
+  },
+  {
+    "revision": "57eff5276f4c4a531696",
+    "url": "/ejemplo-cards/static/js/main.57eff527.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/ejemplo-cards/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/ejemplo-cards/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "ecf1853794a70b6d1a66",
+    "url": "/ejemplo-cards/static/js/runtime~main.ecf18537.js"
+  },
+  {
+    "revision": "46c30677937ee2b38ae3d3861cf3c1f5",
+    "url": "/ejemplo-cards/static/media/card1-front.46c30677.jpg"
+  },
+  {
+    "revision": "df6104bbbe6a1343c9b664d39a2a1bc5",
+    "url": "/ejemplo-cards/static/media/card1-back.df6104bb.jpg"
+  },
+  {
+    "revision": "2f0088a2c30383766ac00e70b1dfe906",
+    "url": "/ejemplo-cards/static/media/card2-front.2f0088a2.jpg"
+  },
+  {
+    "revision": "e5d2e9ba05124f71c4053d2c41ea6eed",
+    "url": "/ejemplo-cards/static/media/card2-back.e5d2e9ba.jpg"
+  },
+  {
+    "revision": "f5c41aa975e285582b6e00579ea9602d",
+    "url": "/ejemplo-cards/static/media/card3-front.f5c41aa9.jpg"
+  },
+  {
+    "revision": "fe95516b42f32d22127c8228a75acf70",
+    "url": "/ejemplo-cards/static/media/card3-back.fe95516b.jpg"
+  },
+  {
+    "revision": "02ef8f3e99751ce2cc300287b18a9d76",
+    "url": "/ejemplo-cards/index.html"
+  }
+];
